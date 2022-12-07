d0ff9615f8c127886a60410dd57d5f8d7fa60ad4
aquib20034 <aquib20034@gmail.com>
2022-12-07 23:26:30 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import React, {useState, useEffect} from "react";
 import './App.css';
 import './Components/Header/Header.css'
 import './Components/QualifyMint/QualifyMint.css'
-// import Header from './Components/Header/Header.js';
-// import QualifyMint from './Components/QualifyMint/QualifyMint.js';
 import AboutPondria from './Components/AboutPondria/AboutPondria.js';
 import RoadMap from './Components/RoadMap/RoadMap.js';
 
@@ -319,8 +317,6 @@ function App() {
 
           {/* END :: QUALIFY-MINT */}
 
-          {/* <Header/> */}
-          {/* <QualifyMint/> */}
           <AboutPondria/>
           <RoadMap/>
         </div>
